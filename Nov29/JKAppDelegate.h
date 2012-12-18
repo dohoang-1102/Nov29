8fc7bc165033f7bfa5e749d93f4477c812127229
Jayesh Kataria <jayeshkataria@gmail.com>
2012-12-17 23:05:06 -0500

    Initial Commit

---
+++ Nov29/JKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JKAppDelegate.h
+//  Nov29
+//
+//  Created by Jayesh Kataria on 12/17/12.
+//  Copyright (c) 2012 Jayesh Kataria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nov29/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nov29
+//
+//  Created by Jayesh Kataria on 12/17/12.
+//  Copyright (c) 2012 Jayesh Kataria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JKAppDelegate class]));
+    }
+}
